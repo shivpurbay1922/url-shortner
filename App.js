d932a391a6a1472399382440d8e9a66dedadbacd
shivpurbay1922 <119876919+shivpurbay1922@users.noreply.github.com>
2025-04-25 06:53:04 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UrlShortener from './pages/UrlShortener';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>URL Shortener</h1>
+      <UrlShortener />
+    </div>
+  );
+}
+
+export default App;
